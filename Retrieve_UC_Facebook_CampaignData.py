c5b1563f1d3fed8e147e9591f5188c5c04dac8f4
lclara01 <113131133+lclara01@users.noreply.github.com>
2022-10-21 14:43:12 -0400

    first commit

---
+++ Retrieve_UC_Facebook_CampaignData.py
@@ -0,0 +1,67 @@
+# Copyright 2014 Facebook, Inc.
+
+# You are hereby granted a non-exclusive, worldwide, royalty-free license to
+# use, copy, modify, and distribute this software in source code or binary
+# form for use in connection with the web services and APIs provided by
+# Facebook.
+
+# As with any software that integrates with the Facebook platform, your use
+# of this software is subject to the Facebook Developer Principles and
+# Policies [http://developers.facebook.com/policy/]. This copyright notice
+# shall be included in all copies or substantial portions of the software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+"""
+Example from Facebook Meta for Develpers:   https://developers.facebook.com/apps/597369678448697/marketing-api/quickstart/
+Modified by L Clarage for Tufts University, 9/21/22.
+"""
+
+
+"""
+WHAT I RUN IN JUPYTER LABS ON MY WINDOWS MACHINE: 
+       %run Retrieve_UC_Facebook_CampaignData.py
+"""  
+
+
+
+
+from facebook_business.adobjects.adaccount import AdAccount
+from facebook_business.adobjects.adsinsights import AdsInsights
+from facebook_business.api import FacebookAdsApi
+
+access_token = 'EAAIfTfd7yDkBAFDxI4C0FfBZCmv2Oobmxe8N9gXIEaM1o5ZBain1gHxTJTtKa1JFoFMNkpGXGJIP7Gi2BCweIhJwPpS8GiSxKRxH8pkvEsKH4n93oIz9DoRzhEUdZBblKSswFJTEDfNIna3NXAwEiuYoqKs3XOz4sWK0YiQBgOS8d4u1GYep8rVVrHDu9YZD'
+ad_account_id = 'act_1755149518150648'
+app_secret = '41705096fcc4faeacdb32b8c22b9b2af'
+app_id = '597369678448697'
+FacebookAdsApi.init(access_token=access_token)
+
+fields = [
+    'account_name',
+    'account_id',
+    'campaign_name',
+    'campaign_id',
+    'date_start',
+    'date_stop',
+    'impressions',
+    'clicks',
+    'spend',
+]
+params = {
+    'time_range': {'since':'2022-09-09','until':'2022-09-16'},
+    'filtering': [],
+    'level': 'campaign',
+    'time_increment': '1',
+}
+print (AdAccount(ad_account_id).get_insights(
+    fields=fields,
+    params=params,
+)
+      )
+
